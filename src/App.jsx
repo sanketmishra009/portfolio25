0b53c103e0785fa647ac6b4c243154a585932e4b
Sanket Mishra <sanketmishra@jerry.local>
2025-01-10 18:25:19 +0530

    navBar finished.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from './sections/NavBar'
+
+const App = () => {
+  return (
+    <div className='bg-black'>
+      <NavBar />
+    </div>
+  )
+}
+
+export default App
